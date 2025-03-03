6293ba22644c6606c857c771faac53774908fa86
[AI11]김재영A <goodikjy@naver.com>
2025-03-03 18:08:27 +0900

    ✨ (Feat): Add CreateUserRequest
    유저 생성 요청 dto 작성

---
+++ src/main/java/study/spring_framework/user/dto/request/CreateUserRequest.java
@@ -0,0 +1,19 @@
+package study.spring_framework.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class CreateUserRequest {
+    @NotNull(message = "email은 필수값입니다.")
+    @Email(message = "email 형식에 맞지 않습니다.")
+    private String email;
+
+    @NotNull(message = "password는 필수값입니다.")
+    @Size(min = 4, max = 20, message = "password는 4~20글자입니다.")
+    private String password;
+}
