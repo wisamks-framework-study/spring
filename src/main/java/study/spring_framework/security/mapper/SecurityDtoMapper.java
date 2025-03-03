abe2c58c5a1ba276aa1b925fa4de0f3dc0b87936
[AI11]김재영A <goodikjy@naver.com>
2025-03-04 00:45:00 +0900

    ✨ (Feat): Add toRegisterResponse in SecurityDtoMapper
    회원가입 응답 dto로 변환하는 함수 작성

--- src/main/java/study/spring_framework/security/mapper/SecurityDtoMapper.java
+++ src/main/java/study/spring_framework/security/mapper/SecurityDtoMapper.java
@@ -2,6 +2,7 @@ package study.spring_framework.security.mapper;
 
 import org.springframework.stereotype.Component;
 import study.spring_framework.security.dto.request.RegisterRequest;
+import study.spring_framework.security.dto.response.RegisterResponse;
 import study.spring_framework.user.dto.request.CreateUserRequest;
 import study.spring_framework.user.dto.response.CreateUserResponse;
 
@@ -12,6 +13,12 @@ public class SecurityDtoMapper {
         return CreateUserRequest.builder()
                 .email(dto.getEmail())
                 .password(dto.getPassword())
+                .build();
+    }
+
+    public RegisterResponse toRegisterResponse(CreateUserResponse dto) {
+        return RegisterResponse.builder()
+                .id(dto.getId())
                 .build();
     }
 }
