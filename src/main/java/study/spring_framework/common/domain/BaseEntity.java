36719154450e2df4750ed0c56c3e377755d245bf
[AI11]김재영A <goodikjy@naver.com>
2025-03-03 16:37:08 +0900

    ✨ (Feat): Add BaseEntity
    entity에 공통으로 들어가는 컬럼들을 담은 BaseEntity 작성

---
+++ src/main/java/study/spring_framework/common/domain/BaseEntity.java
@@ -0,0 +1,36 @@
+package study.spring_framework.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+abstract public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    public void delete() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+    public void restore() {
+        this.deletedAt = null;
+    }
+}
