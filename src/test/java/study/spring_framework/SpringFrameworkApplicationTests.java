d8622e25dbb3588045a42498c3972905c2c106fd
[AI11]김재영A <goodikjy@naver.com>
2025-03-03 14:59:10 +0900

    🎉 (Begin): Initial Commit
    프로젝트 첫 커밋

---
+++ src/test/java/study/spring_framework/SpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
