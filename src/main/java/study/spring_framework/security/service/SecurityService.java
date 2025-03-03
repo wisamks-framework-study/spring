5f5cf5872cdc45188bd56aad988e908fea90ce12
[AI11]김재영A <goodikjy@naver.com>
2025-03-04 00:53:28 +0900

    ✨ (Feat): Add registerUser in SecurityService
    회원가입 서비스 로직 작성

--- src/main/java/study/spring_framework/security/service/SecurityService.java
+++ src/main/java/study/spring_framework/security/service/SecurityService.java
@@ -3,6 +3,10 @@ package study.spring_framework.security.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import study.spring_framework.security.dto.request.RegisterRequest;
+import study.spring_framework.security.dto.response.RegisterResponse;
+import study.spring_framework.security.mapper.SecurityDtoMapper;
+import study.spring_framework.user.dto.response.CreateUserResponse;
 import study.spring_framework.user.service.UserService;
 
 @Service
@@ -11,4 +15,10 @@ import study.spring_framework.user.service.UserService;
 public class SecurityService {
 
     private final UserService userService;
+    private final SecurityDtoMapper securityDtoMapper;
+
+    public RegisterResponse registerUser(RegisterRequest dto) {
+        CreateUserResponse result = userService.createUser(securityDtoMapper.toCreateUserRequest(dto));
+        return securityDtoMapper.toRegisterResponse(result);
+    }
 }
