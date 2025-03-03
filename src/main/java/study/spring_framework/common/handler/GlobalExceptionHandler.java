2544d35b8cb6581f9b3cc325b90478e50b0bca5b
[AI11]김재영A <goodikjy@naver.com>
2025-03-03 23:13:29 +0900

    ✨ (Feat): Add handleValidationException in GlobalExceptionHandler
    유효성 검증 에러 예외 처리 추가

--- src/main/java/study/spring_framework/common/handler/GlobalExceptionHandler.java
+++ src/main/java/study/spring_framework/common/handler/GlobalExceptionHandler.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +16,9 @@ import study.spring_framework.common.exception.ClientException;
 import study.spring_framework.common.mapper.ExceptionResponseMapper;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -25,5 +31,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleClientException(ClientException ex, HttpServletRequest request) {
         logger.warn("클라이언트 에러 발생: {}", ex.getMessage());
         return exceptionResponseMapper.toResponseEntity(ex, request.getRequestURI());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error: fieldErrors) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        logger.warn("유효성 검증 에러 발생: {}", errors);
+        return exceptionResponseMapper.toResponseEntity(errors, request.getRequestURI());
     }
 }
--- src/main/java/study/spring_framework/common/mapper/ExceptionResponseMapper.java
+++ src/main/java/study/spring_framework/common/mapper/ExceptionResponseMapper.java
@@ -1,11 +1,13 @@
 package study.spring_framework.common.mapper;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import study.spring_framework.common.dto.ErrorResponse;
 import study.spring_framework.common.exception.ClientException;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Component
 public class ExceptionResponseMapper {
@@ -19,5 +21,16 @@ public class ExceptionResponseMapper {
                 .path(path)
                 .build();
         return ResponseEntity.status(ex.getHttpStatus()).body(errorResponse);
+    }
+
+    public ResponseEntity<ErrorResponse> toResponseEntity(Map<String, String> errors, String path) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .message(errors.toString())
+                .path(path)
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
