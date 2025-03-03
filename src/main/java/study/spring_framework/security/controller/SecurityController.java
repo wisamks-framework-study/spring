f70577face259f792c3c3e7c6c78a143907ee7ce
[AI11]김재영A <goodikjy@naver.com>
2025-03-04 00:56:13 +0900

    ✨ (Feat): Add registerUser in SecurityController
    회원가입 컨트롤러 작성

--- src/main/java/study/spring_framework/security/controller/SecurityController.java
+++ src/main/java/study/spring_framework/security/controller/SecurityController.java
@@ -1,8 +1,13 @@
 package study.spring_framework.security.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.spring_framework.security.dto.request.RegisterRequest;
+import study.spring_framework.security.dto.response.RegisterResponse;
 import study.spring_framework.security.service.SecurityService;
 
 @RestController
@@ -11,4 +16,11 @@ import study.spring_framework.security.service.SecurityService;
 public class SecurityController {
 
     private final SecurityService securityService;
+
+    @PostMapping("/register")
+    public RegisterResponse registerUser(
+            @Valid @RequestBody RegisterRequest dto
+    ) {
+        return securityService.registerUser(dto);
+    }
 }
