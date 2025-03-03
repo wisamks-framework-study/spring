3a5793b4243b2e83ad8e9de1e3a3a17584b6e9ca
[AI11]김재영A <goodikjy@naver.com>
2025-03-04 00:28:49 +0900

    ♻️ (Refactor): Change CreateUserRequest
    유저 생성 dto에 builder를 이용한 생성자 추가

--- src/main/java/study/spring_framework/user/dto/request/CreateUserRequest.java
+++ src/main/java/study/spring_framework/user/dto/request/CreateUserRequest.java
@@ -3,6 +3,7 @@ package study.spring_framework.user.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,4 +17,10 @@ public class CreateUserRequest {
     @NotNull(message = "password는 필수값입니다.")
     @Size(min = 4, max = 20, message = "password는 4~20글자입니다.")
     private String password;
+
+    @Builder
+    public CreateUserRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
