0335d485d30e2c99078d25c1fa0ccf467251d4af
[AI11]김재영A <goodikjy@naver.com>
2025-03-03 16:43:01 +0900

    ✨ (Feat): Add User
    User 엔티티 생성

--- src/main/java/study/spring_framework/user/domain/User.java
+++ src/main/java/study/spring_framework/user/domain/User.java
@@ -1,4 +1,32 @@
 package study.spring_framework.user.domain;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+import study.spring_framework.common.domain.BaseEntity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "user")
+@SQLRestriction("deleted_at IS NULL")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
 }
